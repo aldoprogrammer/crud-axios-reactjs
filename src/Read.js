b507c73296b8c9212b62a189c50c3bbb2c90b792
aldoprogrammer <aldobesma@gmail.com>
2023-12-09 14:28:53 +0700

    first commit

---
+++ src/Read.js
@@ -0,0 +1,36 @@
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+import { Link, useParams } from 'react-router-dom';
+
+const Read = () => {
+    const [data, setData] = useState([]);
+    const {id} = useParams();
+    useEffect(() => {
+        axios.get(`http://localhost:3000/users/${id}`)
+            .then(res => setData(res.data))
+            .catch(err => console.log(err));
+    }, [id]);
+    
+  return (
+       <div className='d-flex flex-column justify-content-center
+    align-items-center bg-light vh-100'>
+      <h1>Details of Users</h1>
+      <div className='w-75 rounded bg-white border
+      shadow p-4'>
+        <div className='mb-2'>
+            <strong>Name: {data.name}</strong>
+        </div>
+        <div className='mb-2'>
+            <strong>Email: {data.email}</strong>
+        </div>
+        <div className='mb-2'>
+            <strong>Phone: {data.phone}</strong>
+        </div>
+        <Link to={`update/${id}`} className='btn btn-success'>Edit</Link>
+        <Link to='/' className='btn btn-info'>Back</Link>
+      </div>
+    </div>
+  )
+}
+
+export default Read
